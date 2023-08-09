4caec8fac67ac7f105b3ced8d30e20fafbcf167a
sachindsilva16 <sachindsilva16@gmail.com>
2023-08-09 22:49:20 +0530

    Add script.js

--- script.js
+++ script.js
@@ -65,19 +65,90 @@ app.route("/articles")
 
 app.route("/articles/:articleTitle")
 
-.get((req,res)=>{
+    .get((req, res) => {
 
-    Article.findOne({title:req.params.articleTitle}).then(function(foundArticle){
-        if(foundArticle){
-            res.send(foundArticle);
-        } else {
-            res.send("Requested article is not found in the database...");
-        }
-    }).catch(function(err){
-        res.send(err);
+        Article.findOne({ title: req.params.articleTitle }).then(function (foundArticle) {
+            if (foundArticle) {
+                res.send(foundArticle);
+            } else {
+                res.send("Requested article is not found in the database...");
+            }
+        }).catch(function (err) {
+            res.send(err);
+        });
+    })
+
+
+    .post((req, res) => {
+
+        const newArticle = new Article({
+            title: req.params.articleTitle,
+            content: req.body.content
+        });
+
+        newArticle.save().then(() => {
+            res.send("Your article has been saved successfully");
+        }).catch(err => {
+            res.send(err);
+        });
+    })
+
+    .put((req, res) => {
+
+        Article.updateOne(
+            {
+                title: req.params.articleTitle
+            },
+            {
+                $set: {
+                    title: req.body.title,
+                    content: req.body.content
+                }
+
+            },
+
+            {
+                overwrite: true
+            }
+        ).then(() => {
+            res.send("Your data has been succcessfully updated");
+        }).catch(err => {
+            res.send(err);
+        });
+    })
+
+
+    .patch(function (req, res) {
+
+
+        Article.updateOne(
+            {
+                title: req.params.articleTitle
+            },
+            // $set : {title:req.body.title,content:req.body.content}
+            // INSTEAD
+            { $set: req.body }
+        ).then(() => {
+            res.send("Successfully updated article");
+        }).catch(err => {
+            res.send(err);
+        });
+
+    })
+
+    .delete(function(req,res){
+        Article.findOneAndDelete({title:req.params.articleTitle}).then(function(foundArticle){
+            if(foundArticle){
+            res.send("Your article is deleted succesfully");
+
+            } else {
+                res.send("No such article found in the database");
+            }
+            
+        }).catch(function(err){
+            res.send(err);
+        });
     });
-});
-
 
 
 app.listen(PORT, () => {
